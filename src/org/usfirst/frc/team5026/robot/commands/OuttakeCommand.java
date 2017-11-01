4f69e29f94cf653ce08c04066ae3b583e9e1a160
Unknown <james@varah.com>
2017-11-01 16:40:40 -0700

    Fixed Merge Conflicts

--- src/org/usfirst/frc/team5026/robot/commands/OuttakeCommand.java
+++ src/org/usfirst/frc/team5026/robot/commands/OuttakeCommand.java
@@ -8,25 +8,13 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class OuttakeCommand extends Command {
-<<<<<<< HEAD
 	private double power;
     public OuttakeCommand(double power) {
     	requires(Robot.intake);
     	this.power = Math.abs(power);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-=======
-	
-	private double power;
-	
-    public OuttakeCommand(double power) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	this.power = Math.abs(power);
-    	requires(Robot.intake);
->>>>>>> ivanBranch
     }
-
     // Called just before this Command runs the first time
     protected void initialize() {
     }
@@ -49,9 +37,6 @@ public class OuttakeCommand extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-<<<<<<< HEAD
     	Robot.intake.stop();
-=======
->>>>>>> ivanBranch
     }
 }
