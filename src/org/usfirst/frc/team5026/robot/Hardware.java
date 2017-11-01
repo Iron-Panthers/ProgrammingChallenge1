1821f6dc7ea3e8082fce4ba4589b71db120c4326
Unknown <james@varah.com>
2017-11-01 15:53:15 -0700

    Added Arm subsystem for lifting and lowering

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -3,7 +3,8 @@ package org.usfirst.frc.team5026.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Hardware {
-	DoubleSolenoid sol;
+	public DoubleSolenoid sol;
+	
 	public Hardware() {
 		sol = new DoubleSolenoid(RobotMap.SOLENOID_FORWARD, RobotMap.SOLENOID_REVERSE);
 
